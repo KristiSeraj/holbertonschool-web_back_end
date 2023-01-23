009ae4b4d1abc4833cd399f3c9208a7e56f6fb83
KristiSeraj <serajkristi@gmail.com>
2023-01-23 22:05:41 +0100

    test

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns any type or none"""
+    if lst:
+        return lst[0]
+    else:
+        return None
