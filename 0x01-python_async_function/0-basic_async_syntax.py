3321f3436c117936199515f31ada11320d320904
KristiSeraj <serajkristi@gmail.com>
2023-01-24 15:02:08 +0100

    basic of sync

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async module"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Asynchronous coroutine that takes in an integer and waits
+    for a random delay between 0 and max_delay"""
+    random_n = random.uniform(0, max_delay)
+    await asyncio.sleep(random_n)
+    return random_n
