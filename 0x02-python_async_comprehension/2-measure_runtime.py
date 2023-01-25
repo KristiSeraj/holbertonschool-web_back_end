6a41fa0ccc6957fd270130a350ee7bfecdd26d4b
KristiSeraj <serajkristi@gmail.com>
2023-01-25 10:58:56 +0100

    measure runtime of a coroutine

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Measure runtime module"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Executes async_comprehesion four times in parallel using asyncio.gather
+    and measure the total runtime and return it
+    """
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    elapse_time = end_time - start_time
+    return elapse_time
