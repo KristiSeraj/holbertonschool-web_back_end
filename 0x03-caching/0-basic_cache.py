065e79d47bf7842fac0e0073ad1ca45dca3ccce9
KristiSeraj <serajkristi@gmail.com>
2023-01-27 15:13:53 +0100

    first commit

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Caching system"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """Assign to the dictionary cache_data the item for value key"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve the value of key found in cache_data dictionary"""
+        if key and self.cache_data.get(key):
+            return self.cache_data.get(key)
+        return None
