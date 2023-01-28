6f8b8129b51a1ef6243ea800489b61cd82466678
KristiSeraj <serajkristi@gmail.com>
2023-01-28 16:25:25 +0100

    fix

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """MRU Cache Module"""
-import collections
+from collections import deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -9,7 +9,13 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """Init method inheriting from base class"""
         super().__init__()
-        self.deque = collections.deque()
+        self.deque = deque()
+
+    def add_el(self, element):
+        length_deque = len(self.deque)
+        if self.deque[length_deque - 1] != element:
+            self.deque.remove(element)
+            self.deque.append(element)
 
     def put(self, key, item):
         """
@@ -18,19 +24,20 @@ class MRUCache(BaseCaching):
         if len of dictionary is bigger than max_items
         """
         if key and item:
-            if key in self.cache_data:
-                self.deque.remove(key)
-            elif len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                mru = self.deque.pop()
-                del self.cache_data[mru]
-                print(f"DISCARD: {mru}")
-            self.deque.appendleft(key)
             self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                if self.deque:
+                    mru = self.deque.pop()
+                    del self.cache_data[mru]
+                    print(f"DISCARD: {mru}")
+        if key not in self.deque:
+            self.deque.append(key)
+        else:
+            self.add_el(key)
 
     def get(self, key):
         """Retrieve the value of key found in cache_data dictionary"""
-        if key and self.cache_data.get(key):
-            # self.deque.remove(key)
-            # self.deque.appendleft(key)
-            return self.cache_data.get(key)
-        return None
+        el = self.cache_data.get(key, None)
+        if el is not None:
+            self.add_el(key)
+        return el
