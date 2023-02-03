51c28b15f4d111d651d5b89d0c1a6dd341244f84
KristiSeraj <serajkristi@gmail.com>
2023-02-03 14:50:00 +0100

    fixed

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,3 +6,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Returns a hashed password"""
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
