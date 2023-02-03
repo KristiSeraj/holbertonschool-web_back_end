63f79cdfbf52a4a08bddc0ce5685c34dc2fe1340
KristiSeraj <serajkristi@gmail.com>
2023-02-03 14:51:52 +0100

    doc

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,4 +9,5 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if the provided password matches the hashed password"""
     return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
