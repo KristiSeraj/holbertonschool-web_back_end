e8e795053e8dc1b3bffca0acaf7dd5fe92d5c7f7
KristiSeraj <serajkristi@gmail.com>
2023-02-06 14:43:21 +0100

    auth

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Path required"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Header authorization"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Return current user"""
+        return None
