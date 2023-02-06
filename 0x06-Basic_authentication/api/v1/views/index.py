6e23c2249c17026b33605c0b830350fd3be761ae
KristiSeraj <serajkristi@gmail.com>
2023-02-06 14:02:08 +0100

    pycodestyle fixed

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -28,8 +28,5 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-  """GET /api/v1/unauthorized
-  """
-
-  abort(401)
-
+    """GET /api/v1/unauthorized """
+    abort(401)
