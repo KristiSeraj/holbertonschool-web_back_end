c2ef211720252a92d6b3f05b118816f4a1e6d496
KristiSeraj <serajkristi@gmail.com>
2023-02-08 15:36:37 +0100

    test

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -64,8 +64,8 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) and auth.session_cookie(request) is None:
-        return None
+    if auth.authorization_header(request) is None or auth.session_cookie(request) is None:
+        abort(401)
     request.current_user = auth.current_user(request)
 
 
