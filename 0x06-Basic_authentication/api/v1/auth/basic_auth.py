51b4be88d9cc1450020bb619ee10986f2ae2efd2
KristiSeraj <serajkristi@gmail.com>
2023-02-08 12:29:46 +0100

    fix

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Basic auth"""
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,27 @@ class BasicAuth(Auth):
             return None, None
         return decoded_base64_authorization_header.split(":")[0],\
             decoded_base64_authorization_header.split(":")[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns user instance based on his email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance for a request"""
+        self.authorization_header(request)
+        self.extract_base64_authorization_header(request)
+        self.decode_base64_authorization_header(request)
+        self.extract_user_credentials(request)
+        if self.user_object_from_credentials(request, request):
+            return request
