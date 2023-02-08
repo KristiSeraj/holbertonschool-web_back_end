1207f3f345d9d92c15c07a5c74233f9a486cf09e
KristiSeraj <serajkristi@gmail.com>
2023-02-08 15:31:09 +0100

    before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -57,13 +57,15 @@ def before_request():
     if auth is None:
         return
     requested_path = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, requested_path):
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    if auth.authorization_header(request) and auth.session_cookie(request) is None:
+        return None
     request.current_user = auth.current_user(request)
 
 
