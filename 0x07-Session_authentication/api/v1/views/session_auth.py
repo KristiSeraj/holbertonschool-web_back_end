3166c134f653ba1e977eed5d8673fd515570e4c2
KristiSeraj <serajkristi@gmail.com>
2023-02-10 13:41:34 +0100

    doc

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -9,6 +9,7 @@ import os
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
+    """Login request"""
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
