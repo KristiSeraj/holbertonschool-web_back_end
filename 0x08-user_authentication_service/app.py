0d6a5572bc146f6cbd0678c814acc5d67ed1e32d
KristiSeraj <serajkristi@gmail.com>
2023-02-15 13:10:38 +0100

    register end point

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def hello():
     """Message to greet"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """Register a user"""
+    if request.method == 'POST':
+        usr_email = request.form['email']
+        usr_pwd = request.form['password']
+        try:
+            usr = AUTH.register_user(usr_email, usr_pwd)
+            if usr:
+                return jsonify({"email": usr_email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
