69b6cc62bd35fa6e518182b9ab9dbbc9891bf02f
KristiSeraj <serajkristi@gmail.com>
2023-02-15 15:36:00 +0100

    fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -47,3 +47,16 @@ class Auth:
                                       usr.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session
+        """
+        try:
+            usr = self._db.find_user_by(email=email)
+            if usr:
+                usr_id = _generate_uuid()
+                print(usr_id)
+                self._db.update_user(usr.id, session_id=usr_id)
+                print(usr.session_id)
+        except NoResultFound:
+            return None
