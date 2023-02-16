b908f6268e5275ba19a2c7f4970dec55df08ffd4
KristiSeraj <serajkristi@gmail.com>
2023-02-16 14:50:28 +0100

    fix

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from auth import Auth
 
 
@@ -26,7 +26,21 @@ def users():
                 return jsonify({"email": usr_email, "message": "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"})
+        
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Login a user with session id"""
+    if request.method == 'POST':
+        usr_email = request.form['email']
+        usr_password = request.form['password']
+        usr = AUTH.valid_login(usr_email, usr_password)
+        if usr:
+            session_id = AUTH.create_session(usr_email)
+            response = make_response('Setting cookie')
+            response.set_cookie('session_id', session_id)
+            return jsonify({'email': usr_email, 'message': 'logged in'})
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
