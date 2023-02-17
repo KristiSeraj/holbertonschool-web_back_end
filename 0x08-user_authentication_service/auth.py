6527c0cd45afb9f3595579631d5dfa59b0090e5d
KristiSeraj <serajkristi@gmail.com>
2023-02-17 11:01:24 +0100

    fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -75,3 +75,17 @@ class Auth:
         """
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates reset password token
+        """
+        try:
+            usr = self._db.find_user_by(email=email)
+            if not usr:
+                raise ValueError
+            else:
+                rst_token = _generate_uuid()
+                self._db.update_user(usr.id, reset_token=rst_token)
+                return usr.reset_token
+        except Exception:
+            return None
