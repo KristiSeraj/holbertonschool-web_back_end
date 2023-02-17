2e02372756333a6594af10d2adecdf000a0b5327
KristiSeraj <serajkristi@gmail.com>
2023-02-17 13:51:08 +0100

    fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -95,7 +95,8 @@ class Auth:
             usr = self._db.find_user_by(reset_token=reset_token)
             if usr:
                 pwd = _hash_password(password)
-                self._db.update_user(usr.id, reset_token=None, hashed_password=pwd)
+                self._db.update_user(usr.id, reset_token=None,
+                                     hashed_password=pwd)
                 return
         except Exception:
             raise ValueError
