35d3f96a3c4943b179a5c1049260c20288cf1ab6
KristiSeraj <serajkristi@gmail.com>
2023-02-22 14:48:10 +0100

    test client file

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -8,7 +8,7 @@ from unittest.mock import patch
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test githuborgclient class"""
-    @parameterized([
+    @parameterized.expand([
         ('google'),
         ('abc')
     ])
