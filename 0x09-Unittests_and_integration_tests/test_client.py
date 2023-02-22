46ac57b66ecec2dd03341a3669d50971017d9de6
KristiSeraj <serajkristi@gmail.com>
2023-02-22 14:46:20 +0100

    test client file

---
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Test client methods"""
+import unittest
+from client import GithubOrgClient
+from parameterized import parameterized
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test githuborgclient class"""
+    @parameterized([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json', return_value={'payload': True})
+    def test_org(self, name, mock_get):
+        """Test org method"""
+        org = GithubOrgClient(name)
+        self.assertEqual(org.org, mock_get.return_value)
+        mock_get.assert_called_once
