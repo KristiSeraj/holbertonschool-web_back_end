fa3be4673e5c370592292af7ada9d0a5bf6335c8
KristiSeraj <serajkristi@gmail.com>
2023-02-22 11:44:37 +0100

    fix

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """Testing module"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,3 +25,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """Raises KeyError if key doesn't exist"""
         with self.assertRaises(KeyError):
             access_nested_map(mapping, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Get_Json test case class"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Tests get json method"""
+        mock_get.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
