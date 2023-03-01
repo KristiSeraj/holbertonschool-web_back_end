1ad49b9077108fe3ef77c26726eac32989badbdf
KristiSeraj <serajkristi@gmail.com>
2023-03-01 14:56:12 +0100

    fix

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Basic babel app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """Return template 0-index.html which has hello world as h1"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
