df9de800177028fe2ce3ea04d1554b0e9822086e
KristiSeraj <serajkristi@gmail.com>
2023-03-07 10:55:39 +0100

    call history

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -16,6 +16,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Shows the call history"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input = f"{method.__qualname__}:inputs"
+        output = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input, str(args))
+
+        res = method(self, *args, **kwargs)
+
+        self._redis.rpush(output, str(res))
+
+        return res
+    return wrapper
+
+
 class Cache:
     """Cache class"""
     def __init__(self) -> None:
@@ -24,6 +41,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key"""
         key = str(uuid.uuid4())
