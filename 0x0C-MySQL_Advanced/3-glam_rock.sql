5151c39811cee4b8e2d9d53b1d3e9e0953be78a9
KristiSeraj <serajkristi@gmail.com>
2023-03-08 17:19:38 +0100

    fix

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- List glam rock bands
-SELECT band_name, split - formed as lifespan FROM metal_bands WHERE style = "Glam rock" ORDER BY lifespan;
+SELECT band_name, split - formed as lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
