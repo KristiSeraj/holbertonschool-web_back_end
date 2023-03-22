2c9a8358f38bcb32ff4b84f7bcf6556514fd8a3e
KristiSeraj <serajkristi@gmail.com>
2023-03-22 12:55:02 +0100

    fixed return value

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,17 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const userData = createUser();
-
-  photo.then((response) => {
-    process.stdout.write(response.body);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
-
-  userData.then((response) => {
-    console.log(` ${response.firstName} ${response.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()]).then((response) => {
+    console.log(response[0].body, response[1].firstName, response[1].lastName);
   }).catch(() => {
     console.log('Signup system offline');
   });
