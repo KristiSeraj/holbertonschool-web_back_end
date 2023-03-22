002d88bd0861f22245838d54b405ae7cb1d761c4
KristiSeraj <serajkristi@gmail.com>
2023-03-22 21:31:31 +0100

    fix

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,8 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    let param1 = signUpUser(firstName, lastName);
-    let param2 = uploadPhoto(fileName);
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-    console.log([param1, param2]);
+  return Promise.allSettled(promises).then((results) => {
+    results.forEach((result) => console.log(result));
+  });
 }
