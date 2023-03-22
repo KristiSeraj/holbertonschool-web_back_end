eba447c6988f07ab23b41f61b35ebe58063bd1d7
KristiSeraj <serajkristi@gmail.com>
2023-03-22 13:31:04 +0100

    fix

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
-    console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
