fbb2e1fd2b78b6a38eba2f657c9f777120f3da1e
KristiSeraj <serajkristi@gmail.com>
2023-03-24 13:42:32 +0100

    extends

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,15 +1,12 @@
 export default class Building {
   constructor(sqrt) {
-    if (typeof sqrt !== 'number') throw new Error('Sqrt must be a number');
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqrt = sqrt;
   }
 
   get sqrt() {
     return this._sqrt;
-  }
-
-  /*eslint-disable */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
