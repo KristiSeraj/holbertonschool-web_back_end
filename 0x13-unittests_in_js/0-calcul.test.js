72d59365fd4be1d1782aac2719810c75a57c15bf
KristiSeraj <serajkristi@gmail.com>
2023-04-12 12:36:51 +0200

    fix

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -6,4 +6,13 @@ describe('calculateNumber', () => {
     it('should return 4 when a is 1 and b is 3', () => {
         assert.equal(calculateNumber(1, 3), 4);
     });
+    it('should return 5 when a is 1 and b is 3.7', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('should return 5 when a is 1.2 and b is 3.7', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('should return 6 when a is 1.5 and b is 3.7', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
 });
