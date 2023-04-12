91f44eb32dd08c79683b973341c46546f83f311f
KristiSeraj <serajkristi@gmail.com>
2023-04-12 13:19:06 +0200

    fix

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -45,8 +45,40 @@ describe('calculateNumber', () => {
         it('should return -3 when type is SUBTRACT a is 1.2 and b is 4.2', () => {
             assert.equal(calculateNumber('SUBTRACT', 1.2, 4.2), -3);
         })
-        it('should return -4 when type is SUBTRACT a is 1.4 and b is 4.5', () => {
+        it('should return -4 when type is SUBTRACT a is 1.5 and b is 4.5', () => {
             assert.equal(calculateNumber('SUBTRACT', 1.5, 4.6), -3);
+        })
+    })
+    describe('DIVIDE', () => {
+        it('should return 0.25 when type is DIVIDE a is 1 and b is 4', () => {
+            assert.equal(calculateNumber('DIVIDE', 1, 4), 0.25);
+        })
+        it('should return 0.25 when type is DIVIDE a is 1 and b is 4.2', () => {
+            assert.equal(calculateNumber('DIVIDE', 1, 4.2), 0.25);
+        })
+        it('should return 0.2 when type is DIVIDE a is 1 and b is 4.5', () => {
+            assert.equal(calculateNumber('DIVIDE', 1, 4.5), 0.2);
+        })
+        it('should return 0.25 when type is DIVIDE a is 1.2 and b is 4', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.2, 4), 0.25);
+        })
+        it('should return 0.5 when type is DIVIDE a is 1.5 and b is 4', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.5, 4), 0.5);
+        })
+        it('should return 0.25 when type is DIVIDE a is 1.2 and b is 4.2', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.2, 4.2), 0.25);
+        })
+        it('should return 0.4 when type is DIVIDE a is 1.5 and b is 4.5', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.5, 4.6), 0.4);
+        })
+        it('should return Error when type is DIVIDE a is 1.2 and b is 0', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.2, 0), 'Error');
+        })
+        it('should return Error when type is DIVIDE a is 1.5 and b is 0', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.5, 0), 'Error');
+        })
+        it('should return Error when type is DIVIDE a is 1 and b is 0', () => {
+            assert.equal(calculateNumber('DIVIDE', 1, 0), 'Error');
         })
     })
 });
