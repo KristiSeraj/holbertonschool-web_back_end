bd2a4e014f2ad152e43d1a6a87dd865cd8253fee
KristiSeraj <serajkristi@gmail.com>
2023-04-17 16:48:49 +0200

    fix

--- 0x13-unittests_in_js/10-api/api.test.js
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -42,3 +42,36 @@ describe('express server regex integration testing', () => {
         });
     })
 })
+
+
+describe('express server regex integration testing', () => {
+    it('GET /available_payments', (done) => {
+        const call = {
+            url: 'http://localhost:7865/available_payments',
+            method: 'GET'
+        };
+        request(call, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+            done();
+        });
+    })
+})
+
+
+describe('express server regex integration testing', () => {
+    it('POST /login', (done) => {
+        const call = {
+            url: 'http://localhost:7865/login',
+            method: 'POST',
+            json: {
+                userName: 'Betty'
+            }
+        };
+        request(call, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome Betty');
+            done();
+        });
+    })
+})
