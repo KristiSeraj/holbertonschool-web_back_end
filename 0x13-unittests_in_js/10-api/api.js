e7d6f98ff2a6cba7b33beb31d2e3392723d51e4d
KristiSeraj <serajkristi@gmail.com>
2023-04-17 16:31:31 +0200

    api

--- 0x13-unittests_in_js/10-api/api.js
+++ 0x13-unittests_in_js/10-api/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
@@ -9,6 +10,18 @@ app.get('/', (req, res) => {
 
 app.get('/cart/:id(\\d+)', (req, res) => {
     res.send(`Payment methods for cart ${req.params.id}`)
+    console.log('API available on localhost port 7865')
+})
+
+app.get('/available_payments', (req, res) => {
+    res.send({"payment_methods":{"credit_cards":true,"paypal":false}})
+    console.log('API available on localhost port 7865')
+})
+
+
+app.post('/login', (req, res) => {
+    const username = req.body.userName;
+    res.send(`Welcome ${username}`);
     console.log('API available on localhost port 7865')
 })
 
